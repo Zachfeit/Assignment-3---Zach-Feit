1477208be1ca4b780065ca39eb339916b1aa5f05
Zachfeit <118468253+Zachfeit@users.noreply.github.com>
2022-11-30 17:59:54 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,281 @@
+
+  function replace() {
+    document.getElementById("div1").style.display="none";
+    document.getElementById("div2").style.display="flex";
+    document.getElementById("source").style.display="none";
+  }
+
+  var audio = document.getElementById("music1");
+  audio.volume = 0.1;
+
+//question 1
+  const btnb = document.getElementById('btnb');
+
+btnb.addEventListener('click', function onClick() {
+  btnb.style.backgroundColor = 'green';
+  btnb.style.color = 'white';
+});
+
+const btna = document.getElementById('btna');
+
+btna.addEventListener('click', function onClick() {
+  btna.style.backgroundColor = 'salmon';
+  btna.style.color = 'white';
+});
+
+  const btnc = document.getElementById('btnc');
+
+btnc.addEventListener('click', function onClick() {
+  btnc.style.backgroundColor = 'salmon';
+  btnc.style.color = 'white';
+});
+
+function rightb() {
+  document.getElementById("btnd").style.display="flex";
+  document.getElementById("right1");
+  right1.play()
+}
+
+function nextq1() {
+  document.getElementById("div2").style.display="none";
+  document.getElementById("div3").style.display="flex";
+}
+
+//question 2
+const btna2 = document.getElementById('btna2');
+
+btna2.addEventListener('click', function onClick() {
+  btna2.style.backgroundColor = 'green';
+  btna2.style.color = 'white';
+});
+
+const btnb2 = document.getElementById('btnb2');
+
+btnb2.addEventListener('click', function onClick() {
+  btnb2.style.backgroundColor = 'salmon';
+  btnb2.style.color = 'white';
+});
+
+const btnc2 = document.getElementById('btnc2');
+
+btnc2.addEventListener('click', function onClick() {
+  btnc2.style.backgroundColor = 'salmon';
+  btnc2.style.color = 'white';
+});
+
+function righta2() {
+  document.getElementById("btnd2").style.display="flex";
+  document.getElementById("right1");
+  right1.play()
+}
+
+function nextq2() {
+  document.getElementById("div3").style.display="none";
+  document.getElementById("div4").style.display="flex";
+}
+
+//question 3
+const btna3 = document.getElementById('btna3');
+
+btna3.addEventListener('click', function onClick() {
+  btna3.style.backgroundColor = 'green';
+  btna3.style.color = 'white';
+});
+
+const btnb3 = document.getElementById('btnb3');
+
+btnb3.addEventListener('click', function onClick() {
+  btnb3.style.backgroundColor = 'salmon';
+  btnb3.style.color = 'white';
+});
+
+const btnc3 = document.getElementById('btnc3');
+
+btnc3.addEventListener('click', function onClick() {
+  btnc3.style.backgroundColor = 'salmon';
+  btnc3.style.color = 'white';
+});
+
+function righta3() {
+  document.getElementById("btnd3").style.display="flex";
+  document.getElementById("right1");
+  right1.play()
+}
+
+function nextq3() {
+  document.getElementById("div4").style.display="none";
+  document.getElementById("div5").style.display="flex";
+}
+
+//question 4
+const btna4 = document.getElementById('btna4');
+
+btna4.addEventListener('click', function onClick() {
+  btna4.style.backgroundColor = 'salmon';
+  btna4.style.color = 'white';
+});
+
+const btnb4 = document.getElementById('btnb4');
+
+btnb4.addEventListener('click', function onClick() {
+  btnb4.style.backgroundColor = 'green';
+  btnb4.style.color = 'white';
+});
+
+const btnc4 = document.getElementById('btnc4');
+
+btnc4.addEventListener('click', function onClick() {
+  btnc4.style.backgroundColor = 'salmon';
+  btnc4.style.color = 'white';
+});
+
+function rightb4() {
+  document.getElementById("btnd4").style.display="flex";
+  document.getElementById("right1");
+  right1.play()
+}
+
+function nextq4() {
+  document.getElementById("div5").style.display="none";
+  document.getElementById("div6").style.display="flex";
+}
+
+//question 5
+const btna5 = document.getElementById('btna5');
+
+btna5.addEventListener('click', function onClick() {
+  btna5.style.backgroundColor = 'green';
+  btna5.style.color = 'white';
+});
+
+const btnb5 = document.getElementById('btnb5');
+
+btnb5.addEventListener('click', function onClick() {
+  btnb5.style.backgroundColor = 'salmon';
+  btnb5.style.color = 'white';
+});
+
+const btnc5 = document.getElementById('btnc5');
+
+btnc5.addEventListener('click', function onClick() {
+  btnc5.style.backgroundColor = 'salmon';
+  btnc5.style.color = 'white';
+});
+
+function righta5() {
+  document.getElementById("btnd5").style.display="flex";
+  document.getElementById("right1");
+  right1.play()
+}
+
+function nextq5() {
+  document.getElementById("div6").style.display="none";
+  document.getElementById("div7").style.display="flex";
+}
+
+//question 6
+const btna6 = document.getElementById('btna6');
+
+btna6.addEventListener('click', function onClick() {
+  btna6.style.backgroundColor = 'green';
+  btna6.style.color = 'white';
+});
+
+const btnb6 = document.getElementById('btnb6');
+
+btnb6.addEventListener('click', function onClick() {
+  btnb6.style.backgroundColor = 'salmon';
+  btnb6.style.color = 'white';
+});
+
+const btnc6 = document.getElementById('btnc6');
+
+btnc6.addEventListener('click', function onClick() {
+  btnc6.style.backgroundColor = 'salmon';
+  btnc6.style.color = 'white';
+});
+
+function righta6() {
+  document.getElementById("btnd6").style.display="flex";
+  document.getElementById("right1");
+  right1.play()
+}
+
+function nextq6() {
+  document.getElementById("div7").style.display="none";
+  document.getElementById("div8").style.display="flex";
+}
+
+//question 7
+const btna7 = document.getElementById('btna7');
+
+btna7.addEventListener('click', function onClick() {
+  btna7.style.backgroundColor = 'salmon';
+  btna7.style.color = 'white';
+});
+
+const btnb7 = document.getElementById('btnb7');
+
+btnb7.addEventListener('click', function onClick() {
+  btnb7.style.backgroundColor = 'salmon';
+  btnb7.style.color = 'white';
+});
+
+const btnc7 = document.getElementById('btnc7');
+
+btnc7.addEventListener('click', function onClick() {
+  btnc7.style.backgroundColor = 'green';
+  btnc7.style.color = 'white';
+});
+
+function rightc7() {
+  document.getElementById("btnd7").style.display="flex";
+  document.getElementById("right1");
+  right1.play()
+}
+
+function nextq7() {
+  document.getElementById("div8").style.display="none";
+  document.getElementById("div9").style.display="flex";
+}
+
+//question 8
+const btna8 = document.getElementById('btna8');
+
+btna8.addEventListener('click', function onClick() {
+  btna8.style.backgroundColor = 'salmon';
+  btna8.style.color = 'white';
+});
+
+const btnb8 = document.getElementById('btnb8');
+
+btnb8.addEventListener('click', function onClick() {
+  btnb8.style.backgroundColor = 'green';
+  btnb8.style.color = 'white';
+});
+
+const btnc8 = document.getElementById('btnc8');
+
+btnc8.addEventListener('click', function onClick() {
+  btnc8.style.backgroundColor = 'salmon';
+  btnc8.style.color = 'white';
+});
+
+function rightb8() {
+  document.getElementById("btnd8").style.display="flex";
+  document.getElementById("right1");
+  right1.play()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
